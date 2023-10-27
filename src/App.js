88105ca8c31a92d49e76147839bcc15acf5f072c
bstream0138 <bstream0138@gmail.com>
2023-10-27 14:32:42 +0900

    2023-10-27

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import Web3 from 'web3';
 
 function App() {
+  const [web3, setWeb3] = useState();
+  const [account, setAccount] = useState(""); //account 상태 추가
+
+  useEffect(() => {
+    const connectToEthereum = async () => {
+      if (typeof window.ethereum !== "undefined") { // window.ethereum 이 있다면
+        try{
+          // Ethereum 지갑 연결 권한 요청
+          await window.ethereum.enable();
+
+          // Ethereum 객체 초기화
+          const web = new Web3(window.ethereum);
+          setWeb3(web);
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+          console.log("MetaMask 또는 Ethereum 지갑을 설치하고 연결하세요.");
+      }
+    };
+
+    connectToEthereum();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="metaConnect"
+        onClick={() => {
+          connectWallet();
+        }}
+      >
+        connect to MetaMask
+      </button>
+      {/* 연결된 계정 주소를 화면에 출력합니다.*/}
+      <div className="userInfo">주소: {account}</div> 
     </div>
   );
 }
